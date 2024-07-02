87353efe23623757cef4415b86e774fd4c5ba615
seunghyeokleeme <seunghyeok.tech@gmail.com>
2024-07-02 15:02:36 +0900

    feat: .tar 형식 파일 압축 해제

--- data_processing.py
+++ data_processing.py
@@ -1,7 +1,14 @@
 import zipfile
+import tarfile
 
 def extract_zip(zip_file_path, extract_to_path):
     """zip 압축풀기"""
     with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
         zip_ref.extractall(extract_to_path)
+    print(f"{extract_to_path} 경로에 압축을 해제합니다.")
+
+def extract_tar(tar_file_path, extract_to_path):
+    """tar 압축풀기"""
+    with tarfile.open(tar_file_path, 'r') as tar:
+        tar.extractall(extract_to_path)
     print(f"{extract_to_path} 경로에 압축을 해제합니다.")
