74fba7d20e56912f6b02e6daf58430fdde0bb2b0
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2024-07-04 15:19:10 +0900

    feat: 추론 코드 추가 (#25)
    * feat: 모델 평가 기능 추가
    
    * feat: 예측 사진 출력 기능 추가
    
    * feat: xBD 추론 클래스 추가
    
    * feat: 추론 기능 추가

--- dataset.py
+++ dataset.py
@@ -29,3 +29,21 @@ class xBD(keras.utils.Sequence):
 
     return x, y
 
+class xBDInference(keras.utils.Sequence):
+  def __init__(self, batch_size, img_size, input_img_paths, **kwargs):
+      super().__init__(**kwargs)  # 경고 메시지를 해결하기 위한 코드 추가
+      self.batch_size = batch_size
+      self.img_size = img_size
+      self.input_img_paths = input_img_paths
+
+  def __len__(self):
+      return math.ceil(len(self.input_img_paths) / self.batch_size)
+
+  def __getitem__(self, idx):
+      i = idx * self.batch_size
+      batch_input_img_paths = self.input_img_paths[i: i + self.batch_size]
+      x = np.zeros((self.batch_size,) + self.img_size + (3,), dtype='float32')
+      for j, path in enumerate(batch_input_img_paths):
+          img = load_img(path, target_size=self.img_size)
+          x[j] = img
+      return x
